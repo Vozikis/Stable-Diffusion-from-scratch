ccf1d6f4bd0d059d43c1338d7ff914f1aa896609
Vozikis <Vozikisant@gmail.com>
2024-07-11 19:02:44 +0200

    VAE implementation attempt 1

---
+++ encoder.py
@@ -0,0 +1,9 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+class VAE_Encoder(nn.Sequential):
+    def __init__(self,):
+        super().__init__(
+            
+        )
