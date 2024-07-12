c4fa21166c59b5b1062ef6e960357849e4704edb
Vozikis <Vozikisant@gmail.com>
2024-07-12 19:09:27 +0200

    build the decoder

--- decoder.py
+++ decoder.py
@@ -44,3 +44,46 @@ class VAE_ResidualBlock(nn.Module):
         x = self.conv_2(x)
         
         return x + self.residual_layer(residue)
+    
+    
+    class VAE_Decoder(nn.Sequential):
+        def __init__(self):
+            super().__init__(
+                nn.Conv2d(4, 4, kernel_size=1, padding=0),
+                
+                nn.Conv2d(4,512,kernel_size=3,padding=1),
+                
+                VAE_ResidualBlock(512,512),
+                VAE_ResidualBlock(512,512),
+                VAE_ResidualBlock(512,512),
+                
+                nn.Upsample(scale_factor=2),
+                
+                nn.Conv2d(512,512, kernel_size=3, padding=1),
+                
+                VAE_ResidualBlock(512,256),
+                VAE_ResidualBlock(256,256),
+                VAE_ResidualBlock(256,256),
+                
+                nn.Upsample(scale_factor=2),
+                
+                nn.Conv2d(256,256, kernel_size=3, padding=1),
+
+                VAE_ResidualBlock(256,128),
+                VAE_ResidualBlock(128,128),
+                VAE_ResidualBlock(128,128),
+                
+                nn.GroupNorm(32,128),
+                
+                nn.SiLU(),
+                
+                nn.Conv2d(128,3,kernel_size=3,padding=1)
+            )
+        def forward(self, x):
+            scaling = 0.18215
+            x /= scaling
+            
+            for module in self:
+                x=module(x)
+                
+            return x
