e7bdad2cd0ab4a20fc9dc1e831825b10c81591b9
Vozikis <Vozikisant@gmail.com>
2024-07-13 15:09:16 +0200

    create the unet output layer

--- diffusion.py
+++ diffusion.py
@@ -36,6 +36,20 @@ class Upsample(nn.Module):
         x = F.interpolate(x,scale_factor=2, mode='nearest')
         return self.conv(x)
     
+    
+class UNET_OutputLayer(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.groupnorm = nn.GroupNorm(32, in_channels)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+    
+    def forward(self, x):
+        
+        x = self.groupnorm(x)
+        x= F.silu(x)
+        x=self.conv(x)
+        return x
+    
 class UNET(nn.Module):
     def __init__(self):
         super().__init__()
