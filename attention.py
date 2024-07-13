6366f61c687ac20adf601878e715f11c28661264
Vozikis <Vozikisant@gmail.com>
2024-07-13 18:48:59 +0200

    cross attention implementation

--- attention.py
+++ attention.py
@@ -34,3 +34,51 @@ class SelfAttention(nn.Module):
         output = output.reshape(input_shape)
         outpout = self.out_proj(output)
         return outpout
+
+
+class CrossAttention(nn.Module):
+    def __init__(self, n_heads, d_embed, d_cross, in_proj_bias = True, out_proj_bias = True):
+        super().__init__()
+        self.q_proj = nn.Linear(d_embed, d_embed, bias= in_proj_bias)
+        self.k_proj = nn.Linear(d_embed, d_embed, bias= in_proj_bias)
+        self.v_proj = nn.Linear(d_embed, d_embed, bias= in_proj_bias)
+        self.out_proj = nn.Linear(d_embed, d_embed, bias= out_proj_bias)
+        self.n_heads = n_heads
+        self.d_heads = d_embed//n_heads # how much info each head will see
+        
+    def forward(self, x, y):
+        #y is context of the prompt while x is the latent
+        input_shape = x.shape
+        
+        batch_size, sequence_length, d_embed = input_shape
+        
+        interim_shape = (batch_size, -1, self.n_heads, self.d_heads)
+        
+        #multiply by weights
+        q = self.q_proj(x)
+        
+        k = self.k_proj(y)
+        
+        v = self.v_proj(y)
+        
+        q = q.view(interim_shape).transpose(1,2)
+        
+        k = k.view(interim_shape).transpose(1,2)
+        
+        v = v.view(interim_shape).transpose(1,2)
+        
+        weight = q @ k.transpose(-1,-2)
+        
+        weight /= math.sqrt(self.d_heads)
+        #no masking because every pixel can watch every word of the context
+        weight = F.softmax(weight,dim=1)
+        
+        output = weight @ v
+        
+        output = output.transpose(1,2).contiguous()
+        
+        output = output.view(input_shape)
+        
+        output = self.out_proj(output)
+        
+        return output
